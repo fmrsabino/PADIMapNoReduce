9d634ce470cf0ae6f35da70b8fd4aa349c6d8992
fmrsabino <fredx96@gmail.com>
2015-04-26 14:09:09 +0100

    Removed unused file. Usings cleanup.

--- PADIMapNoReduce/PuppetMaster/PuppetMasterForm.cs
+++ PADIMapNoReduce/PuppetMaster/PuppetMasterForm.cs
@@ -1,18 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.Diagnostics;
 using System.Runtime.Remoting;
 using System.Runtime.Remoting.Channels;
 using System.Runtime.Remoting.Channels.Tcp;
+using System.Text.RegularExpressions;
 using System.Threading;
-using System.Diagnostics;
+using System.Windows.Forms;
 
 namespace PuppetMaster
 {
--- PADIMapNoReduce/PuppetMaster/PuppetMasterObject.cs
+++ PADIMapNoReduce/PuppetMaster/PuppetMasterObject.cs
@@ -1,14 +1,6 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Runtime.Remoting.Channels;
-using System.Runtime.Remoting.Channels.Tcp;
-using System.Runtime.Serialization.Formatters;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace PuppetMaster
 {
